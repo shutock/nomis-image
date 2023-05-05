0dd3ed41b7d2f62cabda294bce331ffa3da9313d
shutock <yo@shutock.com>
2023-05-05 17:55:11 +0600

    test

--- src/pages/api/score.tsx
+++ src/pages/api/score.tsx
@@ -11,7 +11,7 @@ export const config = {
 };
 
 const handler: NextApiHandler = async (req) => {
-  const spaceMedium = await fetch.spaceMedium;
+  // const spaceMedium = await fetch.spaceMedium;
   // const spaceBold = await fetch.spaceBold;
   // const helveticaLight = await fetch.helveticaLight;
 
@@ -29,20 +29,20 @@ const handler: NextApiHandler = async (req) => {
   return new ImageResponse(<ScoreImage req={req} ens={ens} />, {
     width: size,
     height: size,
-    fonts: [
-      {
-        data: spaceMedium,
-        name: "font",
-        style: "normal",
-        weight: 500,
-      },
-      // {
-      //   data: helveticaLight,
-      //   name: "font",
-      //   style: "normal",
-      //   weight: 300,
-      // },
-    ],
+    // fonts: [
+    //   {
+    //     data: spaceMedium,
+    //     name: "font",
+    //     style: "normal",
+    //     weight: 500,
+    //   },
+    //   {
+    //     data: helveticaLight,
+    //     name: "font",
+    //     style: "normal",
+    //     weight: 300,
+    //   },
+    // ],
   });
 };
 
