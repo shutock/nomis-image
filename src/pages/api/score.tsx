2c6b213204199979018de990475b6b9018bd5db6
shutock <yo@shutock.com>
2023-05-05 17:54:13 +0600

    test

--- src/pages/api/score.tsx
+++ src/pages/api/score.tsx
@@ -11,9 +11,9 @@ export const config = {
 };
 
 const handler: NextApiHandler = async (req) => {
-  // const spaceMedium = await fetch.spaceMedium;
+  const spaceMedium = await fetch.spaceMedium;
   // const spaceBold = await fetch.spaceBold;
-  const helveticaLight = await fetch.helveticaLight;
+  // const helveticaLight = await fetch.helveticaLight;
 
   const { searchParams } = new URL(req.url!);
   const size = Number(searchParams.get("size")) || 2048;
@@ -29,20 +29,20 @@ const handler: NextApiHandler = async (req) => {
   return new ImageResponse(<ScoreImage req={req} ens={ens} />, {
     width: size,
     height: size,
-    // fonts: [
-    //   {
-    //     data: spaceMedium,
-    //     name: "font",
-    //     style: "normal",
-    //     weight: 500,
-    //   },
-    //   {
-    //     data: helveticaLight,
-    //     name: "font",
-    //     style: "normal",
-    //     weight: 300,
-    //   },
-    // ],
+    fonts: [
+      {
+        data: spaceMedium,
+        name: "font",
+        style: "normal",
+        weight: 500,
+      },
+      // {
+      //   data: helveticaLight,
+      //   name: "font",
+      //   style: "normal",
+      //   weight: 300,
+      // },
+    ],
   });
 };
 
