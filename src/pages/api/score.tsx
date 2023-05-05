cffcbebc6fa851de58170d2668678482602ed61b
shutock <yo@shutock.com>
2023-05-05 18:29:00 +0600

    test

--- src/pages/api/score.tsx
+++ src/pages/api/score.tsx
@@ -11,10 +11,10 @@ export const config = {
 };
 
 const handler: NextApiHandler = async (req) => {
-  // const spaceMedium = await fetch.spaceMedium;
-  // const spaceBold = await fetch.spaceBold;
-  // const interMedium = await fetch.interMedium;
-  // const interLight = await fetch.interLight;
+  const spaceMedium = await fetch.spaceMedium;
+  const spaceBold = await fetch.spaceBold;
+  const interMedium = await fetch.interMedium;
+  const interLight = await fetch.interLight;
 
   const { searchParams } = new URL(req.url!);
   const size = Number(searchParams.get("size")) || 2048;
@@ -27,36 +27,42 @@ const handler: NextApiHandler = async (req) => {
     ens = await getEns(address);
   }
 
-  return new ImageResponse(<ScoreImage req={req} ens={ens} />, {
-    width: size,
-    height: size,
-    // fonts: [
-    //   {
-    //     data: spaceBold,
-    //     name: "font",
-    //     style: "normal",
-    //     weight: 700,
-    //   },
-    //   {
-    //     data: spaceMedium,
-    //     name: "font",
-    //     style: "normal",
-    //     weight: 500,
-    //   },
-    //   {
-    //     data: interMedium,
-    //     name: "font",
-    //     style: "normal",
-    //     weight: 400,
-    //   },
-    //   {
-    //     data: interLight,
-    //     name: "font",
-    //     style: "normal",
-    //     weight: 300,
-    //   },
-    // ],
-  });
+  return new ImageResponse(
+    (
+      // <ScoreImage req={req} ens={ens} />,
+      <>test</>
+    ),
+    {
+      width: size,
+      height: size,
+      fonts: [
+        {
+          data: spaceBold,
+          name: "font",
+          style: "normal",
+          weight: 700,
+        },
+        {
+          data: spaceMedium,
+          name: "font",
+          style: "normal",
+          weight: 500,
+        },
+        {
+          data: interMedium,
+          name: "font",
+          style: "normal",
+          weight: 400,
+        },
+        {
+          data: interLight,
+          name: "font",
+          style: "normal",
+          weight: 300,
+        },
+      ],
+    }
+  );
 };
 
 export default handler;
